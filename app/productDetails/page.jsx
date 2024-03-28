e3d6334fa0f6fba23b7059e2e9b565536c83286b
imdeveloper100 <imdeveloper100@gmail.com>
2024-03-28 10:51:15 +0500

    Product Details Page Updated and Enhanced

--- app/productDetails/page.jsx
+++ app/productDetails/page.jsx
@@ -1,5 +1,10 @@
 "use client";
-import { useState } from "react";
+import React from "react";
+import {
+  CalendarOutlined,
+  RocketOutlined,
+  ClockCircleOutlined,
+} from "@ant-design/icons";
 
 const product = {
   name: "Toyota Hybrid 1200cc",
@@ -28,7 +33,7 @@ const product = {
     },
   ],
   description:
-    "Discover the epitome of automotive excellence with our [Product Name]. Crafted with precision engineering and luxurious design, this car redefines the driving experience. Whether you are cruising through the city streets or embarking on a cross-country adventure, the [Product Name] delivers unparalleled performance and style.",
+    "HONDA CIVIC TRIBORN VTI PROSMATIC UG    ( LEATHER SEATS & NAVIGATION )    MODEL 2014 || REG. 2014(JUNE)     ORIGINAL, SCRATCH LESS, SPOTLESS, OUTCLASS IN JUST LIKE (NEW) REMARKABLE CONDITIONED *ORIGINAL URBAN TITANIUM COLOR*  ONLY (55000) KMS DONE ON *HI-OCTANE*      DVD PLAYER + NAVIGATION    MULTIMEDIA STEERING     AIR PRESS + REMOTE KEYS    ABS BRAKES + AIR BAGS    NEW (EURO STAR) RADIAL TYRES        DEMAND : ( 3,290,000 )",
   highlights: [
     "Engine: [Engine Type and Specs]",
     "Transmission: [Transmission Type and Specs]",
@@ -36,8 +41,22 @@ const product = {
     "Dimensions: [Dimensions Specs]",
     "Weight: [Weight Specs]",
   ],
-  details:
-    "The Corolla Hybrid blends Toyota's renowned reliability with exceptional fuel efficiency, offering a seamless driving experience that's as eco-conscious as it is enjoyable. With its sleek design and advanced hybrid technology, the Corolla Hybrid is the perfect choice for those seeking a greener, more efficient drive without compromising on style or performance.",
+  features: [
+    "ABS",
+    "Air Bags",
+    "Air Conditioning",
+    "CD Player",
+    "DVD Player",
+    "Front Speakers",
+    "Immobilizer Key",
+    "Navigation System",
+    "Power Locks",
+    "Power Mirrors",
+    "Power Steering",
+    "Power Windows",
+    "Steering Switches",
+    "Navigation System",
+  ],
   colors: ["Red", "Black", "Blue", "White", "Grey"],
 };
 
@@ -85,108 +104,113 @@ export default function Example() {
         </nav>
 
         {/* Image gallery */}
-        <div className=" mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-          <div className="border-gray-200 border aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
-            <img
-              src={product.images[0].src}
-              alt={product.images[0].alt}
-              className="h-full w-full object-cover object-center"
-            />
-          </div>
-          <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
-            <div className="border-gray-200 border aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-              <img
-                src={product.images[1].src}
-                alt={product.images[1].alt}
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-            <div className="border-gray-200 border aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-              <img
-                src={product.images[2].src}
-                alt={product.images[2].alt}
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-          </div>
-          <div className="border-gray-200 border aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
+        <div className=" mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div className="mx-2 border-gray-200 border aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden rounded-lg">
             <img
               src={product.images[3].src}
               alt={product.images[3].alt}
               className="h-full w-full object-cover object-center"
             />
           </div>
+          <div className="my-2 border-gray-200 border aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
+            <div className="p-4 mt-4 lg:row-span-3 lg:mt-0">
+              <h2 className="sr-only">Product information</h2>
+              <p className="text-2xl font-bold tracking-tight text-gray-700 sm:text-3xl">
+                {"¥ " + product.price}
+              </p>
+
+              <form className="mt-4">
+                {/* Car Primary Info */}
+                <div class="my-2 grid grid-flow-col justify-stretch">
+                  <div className="border-gray-200 border rounded-lg mx-1 py-2 text-center bg-gray-100">
+                    <CalendarOutlined className="text-2xl text-indigo-500" />{" "}
+                    <p className="mt-2">2014</p>
+                  </div>
+                  <div className="border-gray-200 border rounded-lg mx-1 py-2 text-center bg-gray-100">
+                    <RocketOutlined className="text-2xl text-indigo-500" />{" "}
+                    <p className="mt-2">Petrol</p>
+                  </div>
+                  <div className="border-gray-200 border rounded-lg mx-1 py-2 text-center bg-gray-100">
+                    <ClockCircleOutlined className="text-2xl text-indigo-500" />{" "}
+                    <p className="mt-2">56,000 km</p>
+                  </div>
+                </div>
+                {/* Car Secondary Info */}
+                <div class="my-6 flex justify-normal ">
+                  <div className="flex-initial w-64 border-gray-100 border rounded-lg mx-1 my-auto p-2 ">
+                    <p className="text-sm text-gray-400">Make </p>{" "}
+                    <p className="">Honda</p>
+                  </div>
+                  <div className="flex-initial w-64 border-gray-100 border rounded-lg mx-1 my-auto p-2 ">
+                    <p className="text-sm text-gray-400">Model </p>{" "}
+                    <p className="">Civic Prosmetic</p>
+                  </div>
+                </div>
+
+                <div class="my-6 flex justify-normal ">
+                  <div className="flex-initial w-64 border-gray-100 border rounded-lg mx-1 my-auto p-2 ">
+                    <p className="text-sm text-gray-400">Registration city </p>{" "}
+                    <p className="">Karachi</p>
+                  </div>
+                  <div className="flex-initial w-64 border-gray-100 border rounded-lg mx-1 my-auto p-2 ">
+                    <p className="text-sm text-gray-400">Transmission </p>{" "}
+                    <p className="">Automatic</p>
+                  </div>
+                </div>
+
+                <div class="my-6 flex justify-normal ">
+                  <div className="flex-initial w-64 border-gray-100 border rounded-lg mx-1 my-auto p-2 ">
+                    <p className="text-sm text-gray-400">Assembly </p>{" "}
+                    <p className="">Local</p>
+                  </div>
+                  <div className="flex-initial w-64 border-gray-100 border rounded-lg mx-1 my-auto p-2 ">
+                    <p className="text-sm text-gray-400">Condition </p>{" "}
+                    <p className="">Used (9/10)</p>
+                  </div>
+                </div>
+
+                <button
+                  type="submit"
+                  className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-green-600 px-8 py-3 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 green:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Contact Seller
+                </button>
+              </form>
+            </div>
+            ​
+          </div>
         </div>
 
         {/* Product info */}
-        <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
-          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+
+        <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+          <div className="lg:col-span-2">
             <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
               {product.name}
             </h1>
           </div>
 
-          <div className="mt-4 lg:row-span-3 lg:mt-0">
-            <h2 className="sr-only">Product information</h2>
-            <p className="text-3xl tracking-tight text-gray-900">
-              {"¥ " + product.price}
-            </p>
-
-            <form className="mt-10">
-              <div className="mt-10">
-                <h3 className="text-sm font-medium text-gray-900">Colors:</h3>
-
-                <div className="mt-4">
-                  <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
-                    {product.colors.map((color) => (
-                      <li key={color} className="text-gray-400">
-                        <span className="text-gray-600">{color}</span>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-
-              <button
-                type="submit"
-                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Contact Seller
-              </button>
-            </form>
-          </div>
-
-          <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+          <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
             {/* Description and details */}
             <div>
-              <h3 className="sr-only">Description</h3>
-
               <div className="space-y-6">
                 <p className="text-base text-gray-900">{product.description}</p>
               </div>
             </div>
-
-            <div className="mt-10">
-              <h3 className="text-sm font-medium text-gray-900">
-                Specifications:
-              </h3>
-
-              <div className="mt-4">
-                <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
-                  {product.highlights.map((highlight) => (
-                    <li key={highlight} className="text-gray-400">
-                      <span className="text-gray-600">{highlight}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-
-            <div className="mt-10">
-              <h2 className="text-sm font-medium text-gray-900">Details</h2>
-
-              <div className="mt-4 space-y-6">
-                <p className="text-sm text-gray-600">{product.details}</p>
+            {/* Features Section */}
+            <div className="my-6 flex flex-wrap justify-start">
+              <h2 className="text-xl my-2 font-medium text-gray-900">
+                Features
+              </h2>
+              <div className="flex flex-wrap">
+                {product.features.map((feature) => (
+                  <div
+                    key={feature}
+                    className="max-w-full border-gray-200 border rounded-lg mx-1 my-1 p-2 text-center bg-gray-100 flex-shrink-0"
+                  >
+                    <span className="text-gray-600">{feature}</span>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
